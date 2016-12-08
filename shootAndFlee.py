a383fbf0010ef99565730bec76f513e3ddc1a26c
ammyblabla <bunditnoikiku@gmail.com>
2016-12-08 20:50:13 +0700

    draw player

--- shootAndFlee.py
+++ shootAndFlee.py
@@ -5,15 +5,17 @@ SCREEN_HEIGHT = 600
  
 class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
-        super().__init__(width, height)
- 
-        arcade.set_background_color(arcade.color.BLACK)
- 
- 
+    	super().__init__(width, height)
+
+    	arcade.set_background_color(arcade.color.BLACK)
+    	
+    	self.player = arcade.Sprite('images/rocket1.png')
+    	self.player.set_position(100, 100)
+
     def on_draw(self):
-        arcade.start_render()
- 
- 
+    	arcade.start_render()
+    	self.player.draw()
+
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
